4c98fc93e445fbc18693488232c4883cd1ab03a2
Ole Vegard Mythe Moland <oleveg@gmail.com>
2014-05-04 12:27:56 +0200

    Added visited bool to Node
    -Visited is initialy false
    -Set to true when at least one lowest cost has been calculated for that
    Node
    -Added delegating constructors
    -Changed graph

--- Graph/Node.cpp
+++ Graph/Node.cpp
@@ -9,18 +9,19 @@ struct Node
 	Node() = delete;
 
 	Node ( int ID_ )
+		:	Node( ID_, 0 )
 	{
-		ID = ID_;
-		cost = 999999;
 	}
 
 	Node ( int ID_, int cost_ )
+		:	ID( ID_ )
+		,	cost( cost_ )
+		,	visited( false )
 	{
-		ID = ID_;
-		cost = cost_;
 	}
 	int ID;
 	int cost;
+	bool visited;
 
 	void AddNode( int ID_, int cost_ )
 	{
@@ -28,7 +29,7 @@ struct Node
 	}
 	void Print() const
 	{
-		std::cout << "Node " << ID << " Lowest Cost : " << cost <<  std::endl;
+		std::cout << "Node " << ID << " Lowest Cost : " << cost  <<  std::endl;
 
 		for ( int i = 0 ; i < nodeVec.size() ; ++i )
 		{
@@ -44,11 +45,11 @@ struct Node
 };
 
 // This vector holds all the nodes.
-std::vector< Node > nodeVector =  { {1, 0}, 2, 3 ,4 ,5 ,6 };
+std::vector< Node > nodeVector =  { {1, 0}, 2, 3 ,4 ,5 ,6, 7 };
 
 void AddNodes();
 void PrintGraph();
-void FindPaths( Node node );
+void FindPaths( Node &node );
 
 int main()
 {
@@ -61,32 +62,68 @@ int main()
 	FindPaths( nodeVector[ 3 ] );
 	FindPaths( nodeVector[ 4 ] );
 	FindPaths( nodeVector[ 5 ] );
+	FindPaths( nodeVector[ 6 ] );
 
 	PrintGraph();
 }
-void FindPaths( Node mainNode )
+void FindPaths( Node &mainNode )
 {
-	std::cout << "\nChecking paths from node " << mainNode.ID << std::endl;
-	for ( const auto &node : mainNode.nodeVec )
+	//mainNode.visited = true;
+	std::cout << "Checking paths from node " << mainNode.ID << ", cost : " << mainNode.cost <<  std::endl;
+	for ( auto &node : mainNode.nodeVec )
 	{
-		Node &superNode = nodeVector[ (node.ID - 1 ) ]; 
-		std::cout << "\tChecking min path for node : " << node.ID
-			<< " min cost " << mainNode.cost << " + " << node.cost
-			<< " = " << mainNode.cost + node.cost
-			<< std::endl;
+		// Find current Node in vector
+		Node &originalNode = nodeVector[ (node.ID - 1 ) ]; 
 
-		if ( ( mainNode.cost + node.cost ) < superNode.cost )
+		std::cout << "\tConnected to node : " << node.ID << " with cost " << node.cost << std::endl;
+		std::cout << "\t\tTotal cost " << mainNode.cost << " + " << node.cost << " = " << mainNode.cost + node.cost << std::endl;
+		std::cout << "\t\tOriginal cost " << originalNode.cost << " ID " << originalNode.ID << std::endl;
+
+		if ( !originalNode.visited || ( ( mainNode.cost + node.cost ) < originalNode.cost ) )
 		{
-			std::cout << "\t\tLower!\n";
-			superNode.cost = mainNode.cost + node.cost;
+			std::cout << "\t\t\tLower!\n";
+			originalNode.cost = mainNode.cost + node.cost;
+			originalNode.visited = true;
+			node.visited = true;
 		}
-		else
-			std::cout << "\t\tNot lower!\n";
+		//else std::cout << "\t\tNot lower!\n";
 	}
-
+	std::cin.ignore();
 }
 void AddNodes()
 {
+	nodeVector[0].AddNode( 2, 2 );
+	nodeVector[0].AddNode( 3, 4 );
+	nodeVector[0].AddNode( 4, 1 );
+	nodeVector[0].visited = true;
+
+	nodeVector[1].AddNode( 1, 2 );
+	nodeVector[1].AddNode( 4, 3 );
+	nodeVector[1].AddNode( 5, 10 );
+
+	nodeVector[2].AddNode( 1, 4 );
+	nodeVector[2].AddNode( 4, 2 );
+	nodeVector[2].AddNode( 6, 5 );
+
+	nodeVector[3].AddNode( 1, 1 );
+	nodeVector[3].AddNode( 2, 3 );
+	nodeVector[3].AddNode( 3, 2 );
+	nodeVector[3].AddNode( 5, 2 );
+	nodeVector[3].AddNode( 6, 8 );
+	nodeVector[3].AddNode( 7, 4 );
+
+	nodeVector[4].AddNode( 2, 10 );
+	nodeVector[4].AddNode( 4, 2 );
+	nodeVector[4].AddNode( 7, 6 );
+
+	nodeVector[5].AddNode( 3, 5 );
+	nodeVector[5].AddNode( 4, 8 );
+	nodeVector[5].AddNode( 7, 1 );
+
+	nodeVector[6].AddNode( 4, 4 );
+	nodeVector[6].AddNode( 5, 6 );
+	nodeVector[6].AddNode( 6, 1 );
+/*
 	// Note : Some are duplicates becuase:
 	// 1. Some graphs have different cost in different directions
 	// 2. It makes it easier to travel back and forth
@@ -113,9 +150,11 @@ void AddNodes()
 	//nodeVector[5].AddNode( 1, 14 );
 	//nodeVector[5].AddNode( 3, 2 );
 	nodeVector[5].AddNode( 5, 9 );
+	*/
 }
 void PrintGraph()
 {
+	std::cout << "==================================================================\n";
 	for ( const auto &node : nodeVector )
 		node.Print();
 }
