58e2a469e52504fd6a27cb89679cbf287e86332c
Ole Vegard Mythe Moland <oleveg@gmail.com>
2014-05-20 21:37:54 +0200

    Updated Heap
    -Added template functionality
    -Added PrelocateUp()
    -Iproved Insert() to use PrelocateUp()

--- Exercises/Heap/Heap.h
+++ Exercises/Heap/Heap.h
@@ -3,35 +3,39 @@
 #include <vector> 
 
 typedef std::pair< int32_t, int32_t > IntPair;
+
+template < typename Type >
 class Heap
 {
+
 	public:
 	Heap()
 	{
 		Insert( 31 );
 
-		Insert( 1 ); // 1, parent is ( 1 - 1 ) / 2  = 0
-		Insert( 21 );// 2, parent is ( 2 - 1 ) / 2  = 0
+		Insert( 18 );//  1, parent is ( 1 - 1 ) / 2  = 0
+		Insert( 21 );//  2, parent is ( 2 - 1 ) / 2  = 0
 
-		Insert( 5 ); // 3, parent is ( 3 - 1 ) / 2  = 1
-		Insert( 10 );// 4, parent is ( 4 - 1 ) / 2  = 1
+		Insert( 35 );//  3, parent is ( 3 - 1 ) / 2  = 1
+		Insert( 10 );//  4, parent is ( 4 - 1 ) / 2  = 1
 
-		Insert( 12 );// 5, parent is ( 5 - 1 ) / 2  = 2
-		Insert( 18 );// 6, parent is ( 6 - 1 ) / 2  = 2
+		Insert( 12 );//  5, parent is ( 5 - 1 ) / 2  = 2
+		Insert( 1 ); //  6, parent is ( 6 - 1 ) / 2  = 2
 
-		Insert( 3 );// 7, parent is ( 7 - 1 ) / 2  = 3
-		Insert( 2 );// 8, parent is ( 8 - 1 ) / 2  = 3
+		Insert( 3 ); //  7, parent is ( 7 - 1 ) / 2  = 3
+		Insert( 2 ); //  8, parent is ( 8 - 1 ) / 2  = 3
 
-		Insert( 8 );//  9, parent is (  9 - 1 ) / 2  = 4
-		Insert( 7 );// 10, parent is ( 10 - 1 ) / 2  = 4
+		Insert( 8  );//  9, parent is (  9 - 1 ) / 2  = 4
+		Insert( 28 );// 10, parent is (  9 - 1 ) / 2  = 4
 
+		Insert( 100 );// 11, parent is ( 10 - 1 ) / 2  = 5
+		Insert( 4 );  // 12, parent is ( 10 - 1 ) / 2  = 5
 	}
 	void Print()
 	{
-		int level = 1;
-		for ( int i = 0; i < elements.size() ; ++i )
+		Type level = 1;
+		for ( int32_t i = 0; i < elements.size() ; ++i )
 		{
-
 			if ( i == level )
 			{
 				std::cout << "\n";
@@ -41,37 +45,16 @@ class Heap
 			std::cout << elements[ i ] << "\t";
 		}
 		std::cout << std::endl;
-
-		
 	}
-
-	int ParentOf( int index )
-	{
-		return ( index - 1 ) / 2;
-	}
-	IntPair ChildrenOf( int index )
-	{
-		IntPair intPair;
-		intPair.first = ( index * 2 ) + 1;
-		intPair.second = intPair.first + 1;//( index * 2 ) + 1;
-
-		return intPair;
-	}
-
-	void Insert( int value )
+	void Insert( Type value )
 	{
 		elements.push_back( value );
+		PrelocateUp( elements.size() - 1 );
 	}
-	void PrelocateDown( int index )
+	void PrelocateDown( int32_t index )
 	{
-		std::cout << "===================================== Prelocate Down ( index " << index << " value " << elements[index] << ") =====================================\n";
-		Print();
-
 		if ( index <= 0 || index >= ( elements.size() / 2) )
-		{
-			std::cout << "Done!\n";
 			return;
-		}
 
 		IntPair children = ChildrenOf( index );
 
@@ -79,17 +62,36 @@ class Heap
 
 		if ( elements[ index ] < elements[maxChild] )
 		{
-			std::cout << "Child is larger, change places!\n";
 			std::swap( elements[index], elements[maxChild] );
 			PrelocateDown( maxChild );
 		}
-		else
-		{
-			std::cout << "Child is not larger, do nothing!\n";
-		}
-
 	}
+	void PrelocateUp( int32_t index )
+	{
+		if ( index <= 0  )//|| index >= ( elements.size() / 2) )
+			return;
 
+		int32_t parent = ParentOf( index );
+
+		if ( elements[ index ] > elements[parent] )
+		{
+			std::swap( elements[index], elements[parent] );
+			PrelocateUp( parent );
+		}
+	}
+	private:
+	int32_t ParentOf( int32_t index )
+	{
+		return ( index - 1 ) / 2;
+	}
+	IntPair ChildrenOf( int32_t index )
+	{
+		IntPair intPair;
+		intPair.first = ( index * 2 ) + 1;
+		intPair.second = intPair.first + 1;
+
+		return intPair;
+	}
 	
-	std::vector< int > elements;
+	std::vector< Type > elements;
 };
