231c592f4a021e91b08cde90cfc69922398fc61a
Ole Vegard Mythe Moland <oleveg@gmail.com>
2014-05-27 14:36:28 +0200

    Added Graph represented by adjacency list
    -Main Graph class stores Vertex objects
    -Vertex objects has an ID and a vector if Vertex IDs it's connected to

--- Exercises/Graph/Graph.h
+++ Exercises/Graph/Graph.h
@@ -27,4 +27,79 @@ class Graph_Matrix
 
 	bool matrix[5][5];
 };
+struct Vertex
+{
+	Vertex( int32_t id_ )
+	:	ID( id_ )
+	{
+	}
+
+	void AddEdge( int32_t vertex )
+	{
+		connectedVerticies.push_back( vertex );
+	}
+	bool IsConnected ( int32_t vertex ) const
+	{
+		for ( auto i : connectedVerticies )
+		{
+			if ( i == vertex )
+				return true;
+		}
+
+		return false;
+	}
+	int32_t ID;
+	std::vector< int32_t > connectedVerticies;
+};
+class Graph_AdjList
+{
+	public:
+	Graph_AdjList()
+	{
+	}
+	void AddVertex( int32_t vertex )
+	{
+		adjacencyList.emplace_back( Vertex( vertex ) );
+	}
+
+	void AddEdge( int32_t vertex1, int32_t vertex2 )
+	{
+		for ( auto &p : adjacencyList )
+		{
+			if ( p.ID == vertex1 )
+			{
+				p.AddEdge( vertex2 );
+				return;
+			}
+		}
+		std::cout << "No edge added!\n";
+	}
+
+	bool IsConnected( int32_t vertex1, int32_t vertex2 )
+	{
+		for ( const auto &p : adjacencyList )
+		{
+			if ( p.ID == vertex1 )
+			{
+				if ( p.IsConnected( vertex2 ) )
+				{
+					std::cout << vertex1 << " is connected to " << vertex2 << std::endl;
+					return true;
+				}
+				else
+				{
+					std::cout << vertex1 << " but isn't connected to " << vertex2 << std::endl;
+					return false;
+				}
+
+			}
+		}
+
+		std::cout << vertex1 << " doesnt exist" << std::endl;
+
+		return false;
+	}
+
+	std::vector< Vertex > adjacencyList;
+};
 
