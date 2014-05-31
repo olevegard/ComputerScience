5ff566ec1913a73b02cab2ba76892f3865ca4373
Ole Vegard Mythe Moland <oleveg@gmail.com>
2014-05-31 21:33:11 +0200

    Changed HashTable to use pointers
    -Changed vector to store pointers
    -Changed function to use pointers ( including new in insert )
    -Implented priting of chained values when two variables has the same
    hash ( and thus the same index )

--- Exercises/HashTable/HashTable.h
+++ Exercises/HashTable/HashTable.h
@@ -1,13 +1,36 @@
 #pragma once
 
+struct HashNode
+{
+	HashNode( std::string str )
+		:	HashNode( str, nullptr )
+	{
+	}
+	HashNode( std::string str, HashNode* next_ )
+		:	object( str )
+		,	next( next_ )
+	{
+	}
+
+	bool IsEmpty( )
+	{
+		return ( object.length() == 0 );
+	}
+	void Print()
+	{
+		std::cout << object;
+	}
+	std::string object;
+	HashNode* next;
+};
+
 class HashTable
 {
-public:
+	public:
 	HashTable( )
-	:	maxHash( 100 )
-	,	hashTable( 100 )
+		:	maxHash( 100 )
+		,	hashTable( maxHash, nullptr )
 	{
-
 		Insert( "aaaaaa" );
 		Insert( "bbbbbbb" );
 		Insert( "cccccc" );
@@ -16,26 +39,49 @@ public:
 		Insert( "ThiIsAHshString" );
 		Insert( "WhatValueIsThis" );
 		Insert( "42424242424242" );
+		Insert( "24242424242424" );
+		Insert( "22442244224424" );
 		Insert( "1234567890" );
 		Insert( "abcdefghijklmnopqrstuwxyz" );
 		Insert( "ABCDEFGHIJKLMNOPQRSTUVWXYZ" );
 
+		std::cout << "\n\n";
 		PrintHashTable();
 	}
 	void PrintHashTable()
 	{
+		std::cout << "=============================================== Hash Table ===============================================\n";
 		for ( int32_t i = 0 ; i < hashTable.size() ; ++i )
 		{
-			const auto &str = hashTable[i];
-			if ( str.length() != 0 )
-				std::cout << i << ". " << str << std::endl;
+			if ( hashTable[i] == nullptr )
+				continue;
+
+			std::cout << i;
+
+			PrintAllItems( hashTable[i] );
 		}
+	}
+	void PrintAllItems( HashNode* node )
+	{
+		while ( node )
+		{
+			std::cout << ", " << node->object;
+			node = node->next;
+		}
+		std::cout << std::endl;
 	}
 	void Insert( std::string str )
 	{
 		int32_t index = Hash( str );
 
-		hashTable[ index ] = str;
+		HashNode* node = new HashNode( str );
+
+		if ( hashTable[ index ] != nullptr )
+		{
+			node->next = hashTable[index];
+		}
+
+		hashTable[ index ] = node;
 	}
 	int32_t Hash( const std::string &str )
 	{
@@ -47,10 +93,12 @@ public:
 
 		hashedValue %= maxHash;
 
-		std::cout << "Hash for " << str << " is " << hashedValue << std::endl;
+		std::cout << hashedValue << " hash for " << str << " is " << hashedValue << std::endl;
 
 		return hashedValue;
 	}
 	const int32_t maxHash;
-	std::vector< std::string > hashTable;
+	std::vector< HashNode*> hashTable;
+	private:
+
 };
