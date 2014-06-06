c34a1d7843e2cd42ce6b4d4951de3fdb338cfec6
Ole Vegard Mythe Moland <oleveg@gmail.com>
2014-06-06 14:00:00 +0100

    Change data update

--- changeDate.sh
+++ changeDate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git filter-branch --env-filter '
-if [ $GIT_COMMIT = 01a0fa0a8242a25d19 ]
+if [ $GIT_COMMIT = 01a0fa0a ]
 then
     export GIT_AUTHOR_DATE="Fri Jun 6 12:38:53 2014 -0800"
     export GIT_COMMITTER_DATE="Fri Jun 12 01:01:01 2014 -0700"
